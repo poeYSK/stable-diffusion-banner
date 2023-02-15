8cd0d8e4fb08a76009cbd3c2d63a433a53f71297
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2023-02-15 20:08:41 +0900

    :tada: Initial Commit

---
+++ src/page/last/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LastPage = () => {
+  return (
+    <>
+      <div>LastPage</div>
+    </>
+  );
+};
+
+export default LastPage;
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FirstPage from "../page/first";
+import LastPage from "../page/last";
+import SecondPage from "../page/second";
+
+const Router = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/first" element={<FirstPage />} />
+        <Route path="/second" element={<SecondPage />} />
+        <Route path="/last" element={<LastPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
